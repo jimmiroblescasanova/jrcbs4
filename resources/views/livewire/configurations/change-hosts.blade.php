e16565819a2de999d534a6f1a4f05d99a68f2d8d
Jimmi Robles <55005706+jimmiroblescasanova@users.noreply.github.com>
2021-04-06 23:32:55 -0500

    Update change-hosts.blade.php

--- resources/views/livewire/configurations/change-hosts.blade.php
+++ resources/views/livewire/configurations/change-hosts.blade.php
@@ -9,21 +9,21 @@
             @endif
             <div class="row">
                 <div class="form-group col-12 col-md-8">
-                    <x-forms.input label="URL del hostname" modelName="sql_host" />
+                    <x-forms.input wire:model.lazy="sql_host" name="sql_host">URL del hostname</x-forms.input>
                 </div>
                 <div class="form-group col-12 col-md-4">
-                    <x-forms.input type="number" label="Puerto del hostname" modelName="sql_port" />
+                    <x-forms.input wire:model.lazy="sql_port" type="number" name="sql_port">Puerto del hostname</x-forms.input>
                 </div>
             </div>
             <div class="row">
                 <div class="form-group col-12 col-md-6">
-                    <x-forms.input label="Nombre de la base de datos" modelName="sql_bdd_name" />
+                    <x-forms.input wire:model.lazy="sql_bdd_name" name="sql_bdd_name">Nombre de la base de datos</x-forms.input>
                 </div>
                 <div class="form-group col-12 col-md-3">
-                    <x-forms.input label="Usuario SQL Server" modelName="sql_user" />
+                    <x-forms.input wire:model.lazy="sql_user" name="sql_user">Usuario SQL Server</x-forms.input>
                 </div>
                 <div class="form-group col-12 col-md-3">
-                    <x-forms.input label="Contraseña SQL Server" modelName="sql_pswd" />
+                    <x-forms.input wire:model.lazy="sql_pswd" name="sql_pswd">Contraseña SQL Server</x-forms.input>
                 </div>
             </div>
         </div>
