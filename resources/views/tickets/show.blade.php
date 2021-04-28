a6723b986d74434bcfc0ff5735dfee497da911e2
Jimmi Robles <55005706+jimmiroblescasanova@users.noreply.github.com>
2021-04-28 13:50:03 -0500

    just a test

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -21,10 +21,10 @@
                     <p class="text-muted text-center">{{ $ticket->contact->company->name }}</p>
 
                     <ul class="list-group list-group-unbordered mb-3">
-                        <li class="list-group-item">
+                        {{-- <li class="list-group-item">
                             <b>{{ $ticket->tag->name }}</b> <i class="fas fa-circle float-right"
                                 style="color:{{ $ticket->tag->color }};"></i>
-                        </li>
+                        </li> --}}
                         <li class="list-group-item">
                             <b>Teléfono:</b> <span class="text-muted float-right">{{ $ticket->contact->phone }}</span>
                         </li>
