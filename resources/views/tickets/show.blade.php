e267dbf585ed4e64585baec3b651bf72db9d5b03
Jimmi Robles <55005706+jimmiroblescasanova@users.noreply.github.com>
2021-04-28 14:46:03 -0500

    Duh!

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -22,8 +22,8 @@
 
                     <ul class="list-group list-group-unbordered mb-3">
                         <li class="list-group-item">
-                            <b></b> <i class="fas fa-circle float-right"
-                                style="color:"></i>
+                            <b>{{ $ticket->tag->name }}</b> <i class="fas fa-circle float-right"
+                                style="color:{{ $ticket->tag->color }};"></i>
                         </li>
                         <li class="list-group-item">
                             <b>Teléfono:</b> <span class="text-muted float-right">{{ $ticket->contact->phone }}</span>
